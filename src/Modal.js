e824339a4e80dbf6dc74c789e6f0adefb19f45b8
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-12-03 20:10:24 +0100

    implement modal with react Portal

---
+++ src/Modal.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+
+  const modalRoot = document.getElementById("modal");
+
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+
+    modalRoot.appendChild(elRef.current);
+
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+
+export default Modal;
